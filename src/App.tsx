7e7d99cd7c4b7b290c42174a38b88ac93a1e5fd7
Artoo Deetoo <anselmjs@icloud.com>
2020-12-04 23:09:10 +0530

    basic layou

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="Header">
+        <h1>HRate</h1>
+        <p>Connect your Apple Watch</p>
+      </div>
+      <div className="Content">
+        <text className="BPM-field">80<span className="BPM-units">BPM</span></text>
+      </div>
     </div>
   );
 }
