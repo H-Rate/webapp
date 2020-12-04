5f0c26e3dfd273642d5ee85a7673db312d39a0f4
Artoo Deetoo <anselmjs@icloud.com>
2020-12-05 00:01:11 +0530

    basic bpm display

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,35 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import './App.css';
+import io from 'socket.io-client'
 
 function App() {
+
+  const socket = useRef<null | SocketIOClient.Socket>(null)
+  const [bpm, setBpm] = useState("-")
+
+  useEffect(() => {
+    socket.current = io("http://localhost:3001/")
+    
+    socket.current.on("connect", () => {
+      console.log("connected")
+    })
+
+    socket.current.on("disconnect", () => {
+      console.log("Disconnected")
+    })
+
+    socket.current.on("testevent", (data: number) => {
+      let bpm = Math.round(data * 100)
+      setBpm(bpm.toString())
+    })
+
+    socket.current.connect()
+
+    console.log("socket")
+
+    return () => {}
+  }, [])
+
   return (
     <div className="App">
       <div className="Header">
@@ -9,7 +37,7 @@ function App() {
         <p>Connect your Apple Watch</p>
       </div>
       <div className="Content">
-        <text className="BPM-field">80<span className="BPM-units">BPM</span></text>
+        <p className="BPM-field">{bpm}<span className="BPM-units">BPM</span></p>
       </div>
     </div>
   );
